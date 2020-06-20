7801b64411650a7a74deded9bb68aea3a9743ffd
xiaohu <804091284@qq.com>
2020-06-20 10:00:41 +0800

    first

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -4,6 +4,10 @@
 
 <script>
 export default {
-  name: 'Trademark',
-}
+  name: "Trademark",
+  async mounted() {
+    const result = await this.$API.trademark.getList(1, 5);
+    console.log(result);
+  }
+};
 </script>
