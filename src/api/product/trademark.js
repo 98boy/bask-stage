debd39cbc051db0a05ba582abf1d5e9086a05d99
xiaohu <804091284@qq.com>
2020-06-20 21:58:10 +0800

    6/20

--- src/api/product/trademark.js
+++ src/api/product/trademark.js
@@ -1,4 +1,4 @@
-/* 
+/*
 包含品牌相关接口请求函数
 */
 import request from "@/utils/request"; // 相当于前面我们封装的ajax
@@ -6,25 +6,26 @@ import request from "@/utils/request"; // 相当于前面我们封装的ajax
 const api_name = "/admin/product/baseTrademark";
 
 export default {
-  /* 
+  /*
   获取品牌分页列表
     GET /admin/product/baseTrademark/{page}/{limit}
   获取所有品牌列表
     GET /admin/product/baseTrademark/getTrademarkList
   */
   getList(page, limit) {
-    /* 
+    /*
     return request({
       url: `${api_name}/${page}/${limit}`,
       method: 'get',
     }) */
     if (page && limit) {
       return request.get(`${api_name}/${page}/${limit}`);
+    } else {
+      return request.get(`${api_name}/getTrademarkList`);
     }
-    return request.get(`${api_name}/getTrademarkList`);
   },
 
-  /* 
+  /*
   根据ID获取品牌
   GET /admin/product/baseTrademark/get/{id}
   */
@@ -32,7 +33,7 @@ export default {
     return request.get(`${api_name}/get/${id}`);
   },
 
-  /* 
+  /*
   删除指定id的品牌
     DELETE /admin/product/baseTrademark/remove/{id}
   */
@@ -40,25 +41,25 @@ export default {
     return request.delete(`${api_name}/remove/${id}`);
   },
 
-  /* 
+  /*
   添加品牌
     POST /admin/product/baseTrademark/save
     trademark: 没有id的品牌对象
   */
 
-  remove(trademark) {
+  add(trademark) {
     return request.post(`${api_name}/save`, trademark);
   },
   /* remove({tmName, logoUrl}) {
     return request.post(`${api_name}/save`, {tmName, logoUrl})
   }, */
 
-  /* 
+  /*
   更新品牌
   PUT /admin/product/baseTrademark/update
   trademark: 有id的品牌对象
   */
   update(trademark) {
     return request.put(`${api_name}/update`, trademark);
-  },
+  }
 };
--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,13 +1,250 @@
 <template>
-  <div>Trademark</div>
+  <div>
+    <el-button type="primary" icon="el-icon-plus" @click="showAdd">添加</el-button>
+
+    <el-table style="margin: 20px 0" :data="trademarks" border v-loading="loading">
+      <el-table-column label="序号" type="index" width="80" align="center" />
+
+      <!-- 
+        表格的一列显示的就是数据的属性值: prop来指定
+        如果不是(需要显示一个特定的标签结构): 
+
+      -->
+      <el-table-column prop="tmName" label="品牌名称" />
+
+      <el-table-column label="品牌LOGO">
+        <template slot-scope="scope">
+          <img :src="scope.row.logoUrl" style="width: 100px; height:60px;" />
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作">
+        <template slot-scope="{row, $index}">
+          <el-button type="warning" size="mini" icon="el-icon-edit" @click="showUpdate(row)">修改</el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="remove(row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      @current-change="getTrademarks"
+      @size-change="handleSizeChange"
+      style="text-align: center"
+      :current-page="page"
+      :page-sizes="[3, 6, 9]"
+      :page-size="limit"
+      :total="total"
+      layout="prev, pager, next, jumper, ->, sizes, total"
+    />
+    <el-dialog :title="form.id?'修改品牌':'添加品牌'" :visible.sync="isShowDialog">
+      <el-form :model="form" style="width: 80%">
+        <el-form-item label="品牌名称" label-width="100px">
+          <el-input v-model="form.tmName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="100px">
+          <el-upload
+            class="avatar-uploader"
+            action="/dev-api/admin/product/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过50kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="isShowDialog = false">取 消</el-button>
+        <el-button type="primary" @click="addOrUpdate">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import { trademark } from "../../../api";
 export default {
   name: "Trademark",
-  async mounted() {
-    const result = await this.$API.trademark.getList(1, 5);
-    console.log(result);
+
+  data() {
+    return {
+      trademarks: [], //当前页的品牌列表
+      total: 0, //总数量
+      page: 1, //当前页码
+      limit: 3, //每页的数量
+      isShowDialog: false, // 是否显示添加的对话框
+      form: {
+        // 用来收集品牌添加的数据
+        tmName: "", // 品牌名称
+        logoUrl: "" // 品牌logo的url
+      },
+
+      imageUrl: "", // 上传的图片url
+      loading: false //是否正在请求
+    };
+  },
+
+  mounted() {
+    //  异步获取第一页列表显示
+    this.getTrademarks();
+  },
+  methods: {
+    // 删除指定的品牌
+    remove(trademark) {
+      this.$confirm(`确定删除 ${trademark.tmName} 吗?`, "提示", {
+        type: "warning"
+      })
+        .then(async () => {
+          // 点击确定的回调
+          // 发删除品牌的请求
+          const result = await this.$API.trademark.remove(trademark.id);
+          // 如果成功了, 提示成功, 重新获取列表(哪一页?)
+          if (result.code === 200) {
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+
+            // 哪一页?  显示上一页(当前页的列表数据只剩下1个)  否则显示当前页
+            // 如果当前是第1页且只剩下1条数据 ==> 请求第1页数据(当前页)
+            this.getTrademarks(
+              this.trademarks.length === 1 && this.page > 1
+                ? this.page - 1
+                : this.page
+            );
+          } else {
+            // 如果失败了, 提示删除失败
+            this.$message({
+              type: "error",
+              message: "删除失败"
+            });
+          }
+        })
+        .catch(() => {
+          // 点击取消的回调
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    // 添加或更新
+    async addOrUpdate() {
+      // 准备数据
+      const trademark = this.form;
+      let result;
+      // 提交请求
+      if (trademark.id) {
+        // 更新
+        result = await this.$API.trademark.update(trademark);
+      } else {
+        // 添加
+        result = await this.$API.trademark.add(trademark);
+      }
+      // 请求成功
+      if (result.code === 200) {
+        this.$message.success(`${trademark.id ? "更新" : "添加"}成功!`);
+        // 关闭当前dialog
+        this.isShowDialog = false;
+        // 重新获取列表显示  更新显示当前页\添加显示第一页
+        this.getTrademarks(trademark.id ? this.page : 1);
+      } else {
+        // 请求失败
+        this.$message.success(`${trademark.id ? "更新" : "添加"}失败!`);
+      }
+    },
+    // 上传成功的回调
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+
+    beforeAvatarUpload(file) {
+      // const isJPGOrPNG =
+      //   file.type === "image/jpeg" || file.type === "image/png";
+      const isJPGOrPNG = ["image/jpeg", "image/png"].indexOf(file.type) > 0;
+      const isLt50K = file.size / 1024 < 50;
+
+      if (!isJPGOrPNG) {
+        this.$message.error("上传头像图片只能是 JPG或PNG 格式!");
+      }
+      if (!isLt50K) {
+        this.$message.error("上传头像图片大小不能超过 50k!");
+      }
+      return isJPGOrPNG && isLt50K;
+    },
+    // 显示修改页面
+    showUpdate(trademark) {
+      // 将trademark指定为form
+      // 对trademark进行一个浅拷贝
+      this.form = { ...trademark };
+      // 更新isShowDialog为true
+      this.isShowDialog = true;
+    },
+    // 显示添加页面
+    showAdd() {
+      // 清除form中的数据
+      this.form = {
+        tmName: "",
+        logoUrl: ""
+      };
+      // 显示isShowDialog
+      this.isShowDialog = true;
+    },
+    // 修改每页数量
+    handleSizeChange(pageSize) {
+      // 更新limit
+      this.limit = limit;
+      // 重新回到第一页
+      this.getTrademarks(1);
+    },
+    async getTrademarks(page = 1) {
+      // 更新当前页码
+      this.page = page;
+      this.loading = true;
+      //  获取列表哦数据
+      const result = await this.$API.trademark.getList(page, this.limit);
+      this.loading = false;
+      if (result.code === 200) {
+        const { records, total } = result.data;
+        this.trademarks = records;
+        this.total = total;
+      } else {
+        //失败的提示
+        this.$message({
+          type: "warning",
+          message: "获取分页列表失败"
+        });
+        this.$message.error("获取分页列表失败");
+      }
+    }
   }
 };
 </script>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
